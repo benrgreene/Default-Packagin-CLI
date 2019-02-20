1fd8714c615ffcdf35a0809e364ba32d86014ac2
Ben Greene <benrgreene@gmail.com>
2019-02-20 12:09:19 -0800

    feat: add command for updating default package file

--- modules/UpdatePackage.js
+++ modules/UpdatePackage.js
@@ -7,9 +7,22 @@ const settings  = require("./Settings.js")
 
 module.exports = {
   update: (args) => {
-
+    
   },
   setDefault: (args) => {
-    
+    let file       = args.file ? args.file : 'package.json'
+    let fileExists = shell.test('-f', file)
+    if (fileExists) {
+      let newContent  = shell.cat(file)
+      let shellString = shell.ShellString(newContent)
+      let results     = shellString.to(`${settings.settingsFolder}/${settings.settingsPackageFile}`)
+      if (shellString && results) {
+        console.log(chalk.green('Updated default "package.json" file'))
+      } else {
+        console.log(chalk.red(`Error: couldn't update webpack config file.`))
+      }
+    } else {
+      console.log(chalk.red(`Error: file ${file} doesn\'t exist`))
+    }
   }
 }
